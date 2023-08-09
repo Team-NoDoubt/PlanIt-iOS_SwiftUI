7bd6b51b9d3a7986fa9147578ed84575e0da747b
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-08-09 17:56:15 +0900

    디자인 시스템 추가

---
+++ Application/Sources/EmptyView.swift
@@ -0,0 +1,9 @@
+import PlanItDS
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Text("asd")
+        Color.Primary.skyBlue
+    }
+}
---
+++ DesignSystem/Sources/Color/Primary.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+public extension Color {
+    struct Primary { }
+}
+
+public extension Color.Primary {
+    static let skyBlue: Color = #colorLiteral(red: 0.6587248445, green: 0.8163737655, blue: 0.9035550952, alpha: 1).color
+}
