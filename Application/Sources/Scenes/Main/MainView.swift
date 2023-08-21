b7e53fa379ddb23753c324f3bdd1213280544365
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-08-21 20:30:49 +0900

    tabbar 추가

--- Application/Sources/Scenes/Main/MainView.swift
+++ Application/Sources/Scenes/Main/MainView.swift
@@ -1,9 +1,51 @@
-//
-//  MainView.swift
-//  PlanIt-iOS
-//
-//  Created by 박도연 on 2023/08/18.
-//  Copyright © 2023 com.noubt. All rights reserved.
-//
+import SwiftUI
+import PlanItDS
 
-import Foundation
+struct MainView: View {
+
+    init() {
+        UITabBar.appearance().backgroundColor = .init(Color.GrayScale.gray50)
+    }
+
+    var body: some View {
+        TabView {
+            TimeTableView()
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("시간표")
+                }
+                .tag(0)
+                .onAppear {
+                      print("1")
+                  }
+            ChangesView()
+                .tabItem {
+                    Image(systemName: "arrow.left.arrow.right")
+                    Text("교체내역")
+                }
+                .tag(1)
+                .onAppear {
+                      print("2")
+                  }
+            EvaluationView()
+                .tabItem {
+                    Image(systemName: "pencil")
+                    Text("수행쳥가")
+                }
+                .tag(2)
+                .onAppear {
+                      print("3")
+                  }
+            SeeMoreView()
+                .tabItem {
+                    Image(systemName: "line.3.horizontal")
+                    Text("더보기")
+                }
+                .tag(3)
+                .onAppear {
+                      print("4")
+                  }
+        }
+        .accentColor(Color.Primary.skyBlue)
+    }
+}
