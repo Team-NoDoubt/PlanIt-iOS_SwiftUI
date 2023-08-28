f0555c6ebf9a705185799ae63cad308adade682b
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-08-28 19:44:19 +0900

    TimeTable API

---
+++ Services/TimeTableService/Sources/Data/DataSources/API/TimeTableAPI.swift
@@ -0,0 +1,40 @@
+import Moya
+import Foundation
+import APIModule
+
+enum TimeTableAPI {
+    case fetchTimeTable(grade: Int, classNum: Int)
+}
+
+extension TimeTableAPI: PlanItAPI {
+    
+    var domain: String {
+        return "timetable"
+    }
+    
+    var urlPath: String {
+        switch self {
+        case .fetchTimeTable:
+            return ""
+        }
+    }
+
+    var method: Moya.Method {
+        return .get
+    }
+
+    var errorMap: [Int : Error] {
+        [:]
+    }
+
+    var task: Task {
+        switch self {
+        case .fetchTimeTable(let grade, let classNum):
+            return .requestParameters(parameters:
+                                        [
+                                            "grade": grade,
+                                            "class": classNum
+                                        ], encoding: URLEncoding.queryString)
+        }
+    }
+}
