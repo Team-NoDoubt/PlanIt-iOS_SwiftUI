e39fd8ca14e7dabbc13d932e0a365563f90f0bcb
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-08-28 09:04:11 +0900

    PlanIt API

---
+++ Modules/APIModule/Sources/PlanItAPI.swift
@@ -0,0 +1,34 @@
+import Moya
+import Foundation
+
+public protocol PlanItAPI: TargetType {
+    var domain: String { get }
+    var urlPath: String { get }
+    var errorMap: [Int: Error] { get }
+}
+
+public extension PlanItAPI {
+    var baseURL: URL {
+        return URL(string: "http://3.36.159.7:8080")!
+    }
+
+    var path: String {
+        return domain+urlPath
+    }
+
+    var task: Task {
+        .requestPlain
+    }
+
+    var validationType: ValidationType {
+        return .successCodes
+    }
+
+    var headers: [String: String]? {
+        switch self {
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+
+}
