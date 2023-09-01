7e1dfb410406b3e17ca5b4bf0db68d85c4dcecbe
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-01 17:13:16 +0900

    ✨ :: fetchTimeTableUsecase

---
+++ Services/TimeTableService/Sources/Domain/UseCase/FetchTimeTableUseCase.swift
@@ -0,0 +1,14 @@
+import Foundation
+import RxSwift
+
+public class FetchTimeTableUseCase {
+    private let repository: TimeTableRepository
+
+    init(repository: TimeTableRepository) {
+        self.repository = repository
+    }
+
+    public func excute(grade: Int, classNum: Int) -> Observable<[TimeTableEntity]> {
+        return repository.fetchTimeTableListForWeek(grade: grade, classNum: classNum)
+    }
+}
