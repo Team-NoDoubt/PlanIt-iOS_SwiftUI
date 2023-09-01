c16925861b49faa001c3d784aa28a8cdac18b306
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-01 17:22:18 +0900

    ✨ :: SignInTokenResponse

---
+++ Services/AuthService/Sources/Data/Remote/Response/SigninTokenResponse.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct SigninTokenResponse: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case refreshToken = "refresh_token"
+        case expirationAt = "access_token_exp"
+    }
+
+    let accessToken: String
+    let refreshToken: String
+    let expirationAt: String
+}
