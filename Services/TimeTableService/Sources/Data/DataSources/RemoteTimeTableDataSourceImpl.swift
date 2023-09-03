511171a275fbdd8cea0daceb61ac8cec8b06c90d
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-03 22:20:34 +0900

    🐛 :: request수정

--- Services/TimeTableService/Sources/Data/DataSources/RemoteTimeTableDataSourceImpl.swift
+++ Services/TimeTableService/Sources/Data/DataSources/RemoteTimeTableDataSourceImpl.swift
@@ -7,6 +7,6 @@ import Moya
 class RemoteTimeTableDataSourceImpl: BaseRemoteDataSource<TimeTableAPI>, ReomteTimeTableDataSource {
 
     func fetchTimeTableListForWeek(grade: Int, classNum: Int) -> RxSwift.Single<[TimeTableEntity]> {
-        return request(.fetchTimeTable(grade: grade, classNum: classNum))
+        return request(.fetchTimeTableListForWeek(grade: grade, classNum: classNum))
     }
 }
