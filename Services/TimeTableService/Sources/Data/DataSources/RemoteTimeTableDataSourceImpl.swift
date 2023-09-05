8d102388730a208f7b51e1c0a4cf201619abe0e2
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-05 22:51:18 +0900

    💩 :: 안되는 코드 수정?

--- Services/TimeTableService/Sources/Data/DataSources/RemoteTimeTableDataSourceImpl.swift
+++ Services/TimeTableService/Sources/Data/DataSources/RemoteTimeTableDataSourceImpl.swift
@@ -2,11 +2,13 @@ import Foundation
 
 import BaseModule
 import RxSwift
-import Moya
 
 class RemoteTimeTableDataSourceImpl: BaseRemoteDataSource<TimeTableAPI>, ReomteTimeTableDataSource {
 
-    func fetchTimeTableListForWeek(grade: Int, classNum: Int) -> RxSwift.Single<[TimeTableEntity]> {
+    func fetchTimeTableListForWeek(grade: Int, classNum: Int) -> Single<[TimeTableEntity]> {
         return request(.fetchTimeTableListForWeek(grade: grade, classNum: classNum))
+            .map(TimeTableListResponse.self)
+            .map{ $0.toDomain() }
+
     }
 }
